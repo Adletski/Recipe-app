e6abdc18ec56cd71083ab82063101a96287939e4
Kshilinaa <ksusha_2605@mail.ru>
2024-03-01 00:42:59 +0300

    Реализация экрана с коллекцией рецептов

---
+++ RecipeApp/Recipes/RecipesCategories.swift
@@ -0,0 +1,28 @@
+// RecipesCategories.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Foundation
+
+// MARK: - Types
+
+/// Перечисление всех категорий рецептов
+enum RecipeCategories: String {
+    /// Салаты
+    case salad = "Salad"
+    /// Супы
+    case soup = "Soup"
+    /// Курица
+    case chicken = "Chicken"
+    /// Мясо
+    case meat = "Meat"
+    /// Рыба
+    case fish = "Fish"
+    /// Гарнир
+    case sideDish = "Side Dish"
+    /// Напитки
+    case drinks = "Drinks"
+    /// Блинчики
+    case pancakes = "Pancakes"
+    /// Десерты
+    case desserts = "Desserts"
+}
